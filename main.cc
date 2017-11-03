0407456f03dff4d9bf696fefdf5c49318e6fb61a
André Pedro <andre.lima.pedro@hp.com>
2017-11-03 15:39:12 -0300

    add OpenGL Viewer and cmake

--- main.cc
+++ main.cc
@@ -1,28 +1,71 @@
-#include <chrono>
-#include <cstdio>
-#include <random>
-#include <vector>
+#include <GL/gl.h>
+#include <GL/glut.h>
 
 #include "qhull.h"
 
-int main() {
+void Resize(int width,int height)
+{
+  glViewport(0,0,width,height);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  gluPerspective(60.,(GLdouble)width/(GLdouble)height,1,100);
+}
+
+void sortByConnectPoints(std::vector<Point>& hullPoints)
+{
+  Point tmp;
+  for (int i = 0; i < hullPoints.size() -1; ++i)
+  {
+    int j= i+ 1;
+    for (; j < hullPoints.size(); ++j)
+    {
+      bool b = false;       
+      for (int k = i+1; k < hullPoints.size() && !b; ++k)
+        b = (j != k) && SideOfLine(hullPoints[i], hullPoints[j], hullPoints[k]) > 0;
+      if(!b) break;
+    }
+    if(j != i +1)
+    {
+      tmp = hullPoints[i+1];
+      hullPoints[i+1] = hullPoints[j];
+      hullPoints[j] = tmp;
+    }
+  } 
+}
+
+void drawQHull()
+{
+    //Generate and obtain Points
   const size_t number_of_points = 100;
   std::vector<Point> points;
   points.reserve(number_of_points);
   std::vector<Point> hullPoints;
 
-  // Random number generator
-  std::mt19937 mersenne_twister(std::chrono::high_resolution_clock::now().time_since_epoch().count());
-
-  // Generate random points
-  for (int i = 0; i < number_of_points; ++i) {
+  unsigned int seed(0);
+  for(int i=0;i<number_of_points;i++)
+  {
     Point pt;
-    pt.x = (float)(mersenne_twister() % 1000);
-    pt.y = (float)(mersenne_twister() % 1000);
+    pt.x = 25 + (rand_r(&seed)%300);
+    pt.y = 25 + (rand_r(&seed)%300);
+
     points.push_back(pt);
   }
 
-  // Sort points so that we can find min and max
+
+  glPointSize(5);
+
+    //Red color for inside points
+  glColor3f(1,0,0);
+  glBegin(GL_POINTS);
+  for(int i=0;i<points.size();i++)
+  {
+    glVertex2f(points[i].x,points[i].y);
+  }
+  glEnd();
+
+
+
+   // Sort points so that we can find min and max
   std::sort(points.begin(), points.end());
   // Find convex hull points and place them into hullPoints
   QuickHull(points, hullPoints);
@@ -44,4 +87,51 @@ int main() {
     }
     fclose(fp);
   }
+
+  glColor3f(0,1,0);
+  glBegin(GL_POINTS);
+  for(int i=0;i<hullPoints.size();i++)
+  {
+    glVertex2f(hullPoints[i].x,hullPoints[i].y);
+  }
+  glEnd();  
+  sortByConnectPoints(hullPoints);
+  int j;
+  for(int i=0;i<hullPoints.size();++i)
+  {
+    j = (i+1)%hullPoints.size();
+    glLineWidth(2.5);
+    glColor3f(0,1,0);
+    glBegin(GL_LINES);
+    glVertex2f(hullPoints[i].x,hullPoints[i].y);
+    glVertex2f(hullPoints[j].x,hullPoints[j].y);
+    glEnd();
+  }
+}
+
+void display()
+{
+  glLoadIdentity();
+  glOrtho( 0,400,400,0,0.0f,100.0f);
+  glClearColor( 1,1,1,1);
+  glClearDepth( 1.0f);
+  glClear(GL_COLOR_BUFFER_BIT |GL_DEPTH_BUFFER_BIT);
+
+    //display the Quick hull process
+  drawQHull();
+
+  glutSwapBuffers();
+}
+
+int main(int argc,char* argv[])
+{
+  glutInit(&argc,argv);
+  glutInitWindowSize(400,400);
+  glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGB|GLUT_DEPTH);
+  glutCreateWindow("Quick Hull 2d Implementation");
+  glutDisplayFunc(display);
+  glutReshapeFunc(Resize);
+
+  glutMainLoop();
+  return 0;
 }
